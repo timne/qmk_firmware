99ba5dc5f6072f23a69ce814c526a532227cd48b
Tim Nelson <timnelson94@gmail.com>
2020-09-03 18:24:47 -0500

    update pan config and fix led order in pan.c

--- keyboards/rgbkb/pan/rev1/proton_c/config.h
+++ keyboards/rgbkb/pan/rev1/proton_c/config.h
@@ -21,4 +21,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { A8, A6, B0, B1, B2, B4, B5, A1, A2, B3, B9, A10, A9 }
 #define ENCODERS_PAD_A { B14, B11 }
 #define ENCODERS_PAD_B { B15, B12 }
+#define DIP_SWITCH_PINS { A0, B13 }
 #define RGB_DI_PIN A3
+#define ENCODER_DIRECTION_FLIP
+#define TAP_CODE_DELAY 15
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
+
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+#define RGB_MATRIX_CENTER { 104, 42 }
+
+#define RGB_MATRIX_HUE_STEP 7
+#define RGB_MATRIX_SAT_STEP 13
+#define RGB_MATRIX_VAL_STEP 13
+#define RGB_MATRIX_SPD_STEP 13
